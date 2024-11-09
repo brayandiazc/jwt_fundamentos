ce02fc20555504435e3be9a9fb74178feed552ef
Javilani <javilagosnieto@gmail.com>
2024-11-09 10:39:28 -0300

    doc: documentacion del index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Importamos librerias
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Usamos variables de entorno dotenv
 dotenv.config();
 
+// Usamos express y retornar un json
 const app = express();
 app.use(express.json());
 
+// Importa las rutas de ./auth
 const authRoutes = require("./auth");
 app.use("/api/auth", authRoutes);
 
+// Definimos puerto
 const PORT = process.env.PORT || 3000;
 
+// Escuchamos al PORT y regresa un console.log con las rutas
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
   console.log(`Rutas disponibles:`);
